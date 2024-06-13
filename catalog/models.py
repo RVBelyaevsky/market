132fc08fb4d98a1653c8944481a63a0e5dabed42
RVBelyaevsky <Rooman83@mail.ru>
2024-06-13 15:48:55 +0800

    homework 20.2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True,
+            'null': True}
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='Наименование категории')
+    category_description = models.TextField(verbose_name='Описание категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    product_description = models.TextField(verbose_name='Описание продукта')
+    product_image = models.ImageField(upload_to='products/', verbose_name='Изображение продукта', **NULLABLE)
+    product_category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория продукта', **NULLABLE,
+                                         related_name='categories')
+    product_price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_name',)
+
+    def __str__(self):
+        return self.product_name
+
+
+
