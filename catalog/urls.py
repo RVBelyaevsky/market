41c9098ad17e635880f264cc276b8dc40e92383e
RVBelyaevsky <rooooooma73@gmail.com>
2024-07-11 16:14:33 +0800

    сделал урлы и контроллеры

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, product
+from catalog.views import CatalogListView, CatalogDetailView, ContactTemplateView, BlogCreateView, BlogDetailView, \
+    BlogUpdateView, BlogDeleteView, BlogListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', product, name='product')
+    path('', CatalogListView.as_view(), name='index'),
+    path('contacts/', ContactTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>/', CatalogDetailView.as_view(), name='product'),
+
+    path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/create', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/edit/<int:pk>', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,29 +1,50 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
-from catalog.models import Product
-
-
-def index(request):
-    products = Product.objects.all()
-    context = {'products': products,
-               'title': 'Каталог'}
-    return render(request, 'catalog/home.html', context)
+from catalog.models import Product, Blog
 
 
-def contacts(request):
-    if request.method == 'POST':
+class CatalogListView(ListView):
+    model = Product
+
+
+class CatalogDetailView(DetailView):
+    model = Product
+
+
+class ContactTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'User:{name}(phone number:{phone}) send message: "{message}"')
 
-    context = {'title': 'Контакты'}
-
-    return render(request, 'catalog/contacts.html', context)
+        return render(request, 'catalog/contacts.html')
 
 
-def product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    # product = Product.objects.get(pk=pk)
-    context = {'product': product}
-    return render(request, 'catalog/product.html', context)
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('blog_title', 'blog_slug', 'blog_content', 'blog_image', 'is_published')
+    success_url = reverse_lazy('catalog:blog_list')
+
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content',)
+    success_url = reverse_lazy('catalog:blog_list')
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('catalog:blog_list')
